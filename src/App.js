93c759ded39e48d62f3fe31b4fd07a61b9877a1b
tycoon <smizidor@gmail.com>
2020-04-24 14:40:29 +0200

    routing logic added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
@@ -21,24 +20,13 @@ class App extends React.Component {
   };
 
   render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-        <Page1 onRouteChange={this.onRouteChange} />
-        <Page2 onRouteChange={this.onRouteChange} />
-        <Page3 onRouteChange={this.onRouteChange} />
-      </div>
-    );
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page2') {
+      return <Page2 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page3') {
+      return <Page3 onRouteChange={this.onRouteChange} />;
+    }
   }
 }
 
