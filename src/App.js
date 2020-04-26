993e6def634339b5bdeaf2dd3493c11f1a6fe043
tycoon <smizidor@gmail.com>
2020-04-26 08:28:17 +0200

    imports removed from top

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import Page3 from './components/Page3';
 //import Page2 from './components/Page2';
 //import Page3 from './components/Page3';
 
